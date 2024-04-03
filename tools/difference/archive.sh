fda71012aa4abbc337b17637fa4458463a561d57
maru-koyo <k.takaya@marukin-ad.co.jp>
2024-04-03 10:39:27 +0900

    save

--- tools/difference/archive.sh
+++ tools/difference/archive.sh
@@ -1,5 +1,7 @@
-name="sushi"
 time=`date "+%Y%m%d-%H_%M"`
-mixName="$time-$name"
+CURRENT=$(cd $(dirname $0);pwd)
+DIRNAME=$(echo "$CURRENT" | awk -F'/' '{print $(NF-2)}')
+
+mixName="$time-$DIRNAME"
 
 git archive --prefix=$mixName/ main `git diff --name-only origin/main main --diff-filter=d` -o tools/difference/$mixName.zip
