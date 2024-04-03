77789f25df74de00875e3766fd6997752d8e3537
maru-koyo <k.takaya@marukin-ad.co.jp>
2024-04-03 10:59:05 +0900

    v

--- tools/difference/archive.sh
+++ tools/difference/archive.sh
@@ -14,11 +14,12 @@
 
 # git archive --prefix="$mixName/" main "$(git diff --name-only origin/main main --diff-filter=d -- src/)" -o "tools/difference/$mixName.zip"
 
-
 time=$(date "+%Y%m%d-%H_%M")
 CURRENT=$(cd "$(dirname "$0")" || exit; pwd)
 DIRNAME=$(echo "$CURRENT" | awk -F'/' '{print $(NF-2)}')
 
 mixName="$time-$DIRNAME"
 
-git archive --prefix="$mixName/" main "$(git diff --name-only origin/main main --diff-filter=d -- 'src/'*)" -o "tools/difference/$mixName.zip"
+git diff --name-only origin/main main --diff-filter=d -- 'src/'* | sed 's|^src/||' | while read -r file; do
+  git archive --prefix="$mixName/" main "src/$file"
+done | zip -q -@ "tools/difference/$mixName.zip"
