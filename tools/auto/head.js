6edccc49a7e35df2c3536329a6825c52265dcae3
maru-koyo <k.takaya@marukin-ad.co.jp>
2024-04-04 09:46:23 +0900

    a

--- tools/auto/head.js
+++ tools/auto/head.js
@@ -1,12 +1,11 @@
-import { insertHead } from './components/insertHead.js'
+import { insertHead } from "./components/insertHead.js";
 // ページごとに変化がなく、全ページ共通で入れるタグはSSIなどでファイル分割する方が良い->更新時にすべてのファイルをftpにアップする必要ないから
 
 export function head(site, currentPage, page, count, fileName) {
-  const isFrontPage =
-    count === 1 && fileName === 'index.html' ? 'website' : 'article'
+  const isFrontPage = count === 1 && fileName === "index.html" ? "website" : "article";
   const data = `<html lang="ja">
 <head prefix="og: https://ogp.me/ns# fb: https://ogp.me/ns/fb# ${isFrontPage}: https://ogp.me/ns/${isFrontPage}#">
-<!--#include virtual="/common/include/header.html" -->
+<!--#include virtual="/common/includes/head.html" -->
 <meta property="og:site_name" content="${site.name}" />
 <title>${currentPage.title}</title>
 <meta name="description" content="${currentPage.description}" />
@@ -15,6 +14,6 @@ export function head(site, currentPage, page, count, fileName) {
 <meta property="og:type" content="${isFrontPage}" />
 <meta property="og:url" content="${site.url + page}" />
 <link rel="canonical" href="${site.url + page}">
-`
-  return insertHead(data)
+`;
+  return insertHead(data);
 }
